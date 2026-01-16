c8a635808cc5c356ebc992064dbd16d524a579a2
joão gabriel abreu soares <jgabreusoares@gmail.com>
2026-01-16 19:23:21 -0300

    Adicionando função de comunicação de 2 vias.

--- comunicacao em socket/client.py
+++ comunicacao em socket/client.py
@@ -2,9 +2,12 @@ import socket
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-cliente.connect(("localhost", 1986))
+cliente.connect(("localhost", 1986)) #Conecta no IP do server e a porta a ser escolhida.
+conn, addr = cliente, ("localhost", 1986) #Simulando o mesmo comportamento do server para printar o endereco.
 
-while True:
-
+while True:   
     msg = input("Digite aqui sua mensagem: ")
-    cliente.send(msg.encode())
+    conn.send(msg.encode()) #
+
+    resposta = conn.recv(1024)
+    print("Servidor: %s: %s" %(addr, resposta.decode())) 
--- comunicacao em socket/server.py
+++ comunicacao em socket/server.py
@@ -1,5 +1,4 @@
 import socket 
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Criando objeto socket para receber dados de um socket client.
 
 server.bind(("localhost", 1986)) #configure para colocar o seu ip e uma porta qualquer acima de 1024.
@@ -8,9 +7,7 @@ server.listen()
 conn, addr = server.accept()#Ouve a porta e so entao se conecta
 
 while True:
-
+    msg = input("Digite aqui sua mensagem:")
+    conn.send(msg.encode())
     resposta = conn.recv(1024)
-    if not resposta:
-        break
-
     print("Mensagem do Cliente: %s: %s" %(addr, resposta.decode())) #Passando a mensagem de Bytes para Str com o metodo ".decode".
